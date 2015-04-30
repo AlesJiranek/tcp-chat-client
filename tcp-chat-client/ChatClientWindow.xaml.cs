a3b31d570c4f817ed93573560c790ea4e0c54411
Aleš Jiránek <a.jiranek@gmail.com>
2015-04-30 17:21:04 +0200

    Choose chatroom window

--- tcp-chat-client/ChatClientWindow.xaml.cs
+++ tcp-chat-client/ChatClientWindow.xaml.cs
@@ -23,14 +23,15 @@ namespace tcp_chat_client
     /// </summary>
     public partial class ChatClientWindow : Window
     {
-        private TcpClient serverSocket;
-        private bool isConnected = false;
+        private const String CREATE_NEW_TEXTROOM_TEXT = "Create a new chatroom...";
+        private Client client;
 
 
         public ChatClientWindow()
         {
-            Connect();
-            if (!this.isConnected)
+            this.client = new Client();
+
+            if (!this.client.Connect())
             {
                 MessageBox.Show("Unable to connect chat server! Server is not turned on probably.");
                 Environment.Exit(1);
@@ -43,84 +44,71 @@ namespace tcp_chat_client
 
         ~ChatClientWindow()
         {
-            this.Disconnect();
-        }
-
-        protected void Connect()
-        {
-            String ip = "127.0.0.1";
-            int port = 7777;
-
-            if (this.isConnected)
-            {
-                return;
-            }
-
-            IPAddress ipAddress = IPAddress.Parse(ip);
-            try
-            {
-                this.serverSocket = new TcpClient();
-                this.serverSocket.Connect(ip, port);
-                this.isConnected = true;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Error: " + e.Message);
-                this.isConnected = false;
-            }
-        }
-
-        protected void Disconnect()
-        {
-            if (!this.isConnected)
-            {
-                return;
-            }
-
-            this.serverSocket.Close();
-            this.isConnected = false;
-        }
-
-        protected void SendMessage(String message)
-        {
-            if (!this.isConnected)
-            {
-                return;
-            }
-
-            NetworkStream stream = this.serverSocket.GetStream();
-            StreamWriter writer = new StreamWriter(stream);
-            writer.WriteLine(message);
-            writer.Flush();
-        }
-
-        private void SendMessageButton_Click(object sender, RoutedEventArgs e)
-        {
-            this.SendMessage("[" + DateTime.Now.ToString() + "]: Ahoj");
-        }
-
-        private String receiveMessage()
-        {
-            NetworkStream stream = serverSocket.GetStream();
-            StreamReader reader = new StreamReader(stream);
-            String message = reader.ReadLine();
-
-            return message;
+            this.client.Disconnect();
         }
 
         private void showChatroomsList()
         {
-            String roomsString = receiveMessage();
+            String roomsString = this.client.receiveMessage();
             List<String> chatRooms = roomsString.Split(';').ToList();
+            chatRooms.Add(CREATE_NEW_TEXTROOM_TEXT);
             this.ChatRoomsListBox.ItemsSource = chatRooms;
         }
 
+
         private void ChatRoomsListBox_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            InputDialog dialog = new InputDialog("Enter your name:");
+            var tb = (TextBlock)e.OriginalSource;
+            String chatroomName = tb.Text;
 
-            if(dialog.ShowDialog() == true)
-                Console.WriteLine(dialog.getAnswer());
+            if (chatroomName.Equals(CREATE_NEW_TEXTROOM_TEXT))
+            {
+                // Show dialog to enter new chatroom name
+                InputDialog chatroomNameDialog = new InputDialog("Enter name of new chatroom:");
+                chatroomNameDialog.Title = "Enter Chatroom Name";
+                if (chatroomNameDialog.ShowDialog() == true)
+                {
+                    chatroomName = chatroomNameDialog.getAnswer();
+                }
+                else
+                {
+                    return;
+                }
+
+                // Check if chatroom with same name already exists
+                // Items.Contains() doesn't support case insensitive comparsion
+                if (this.ChatRoomsListBox.Items.OfType<string>().Contains(chatroomName,StringComparer.OrdinalIgnoreCase))
+                {
+                    MessageBox.Show("Chatroom with name \"" + chatroomName + "\" already exists.");
+                    return;
+                }
+
+                // Check if entered name contains only valid characters
+                if (!this.client.ValidateName(chatroomName))
+                {
+                    MessageBox.Show("Invalid chatroom name!");
+                    return;
+                }
+            }
+
+
+            // Show dialog to enter username
+            InputDialog userNameDialog = new InputDialog("Enter your name:");
+            userNameDialog.Title = "Enter Name";
+            if (userNameDialog.ShowDialog() == true)
+            {
+                String username = userNameDialog.getAnswer();
+
+                // Check if entered name contains only valid characters
+                if (!this.client.ValidateName(username))
+                {
+                    MessageBox.Show("Invalid name!");
+                    return;
+                }
+
+                this.client.SendMessage(chatroomName);
+                this.client.SendMessage(username);
+            }
         }
     }
 }
---
+++ tcp-chat-client/Client.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace tcp_chat_client
+{
+    class Client
+    {
+        private TcpClient serverSocket;
+        private bool isConnected = false;
+
+        public bool Connect()
+        {
+            String ip = "127.0.0.1";
+            int port = 7777;
+
+            if (this.isConnected)
+            {
+                return true;
+            }
+
+            IPAddress ipAddress = IPAddress.Parse(ip);
+            try
+            {
+                this.serverSocket = new TcpClient();
+                this.serverSocket.Connect(ip, port);
+                this.isConnected = true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+                this.isConnected = false;
+            }
+
+            return this.isConnected;
+        }
+
+        public void Disconnect()
+        {
+            if (!this.isConnected)
+            {
+                return;
+            }
+
+            this.serverSocket.Close();
+            this.isConnected = false;
+        }
+
+        public void SendMessage(String message)
+        {
+            if (!this.isConnected)
+            {
+                return;
+            }
+
+            NetworkStream stream = this.serverSocket.GetStream();
+            StreamWriter writer = new StreamWriter(stream);
+
+            writer.WriteLine(message);
+            writer.Flush();
+        }
+
+        public String receiveMessage()
+        {
+            NetworkStream stream = serverSocket.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            String message = reader.ReadLine();
+
+            return message;
+        }
+
+        public bool ValidateName(String name)
+        {
+            Regex reg = new Regex(@"^[a-zA-Zěščřžýáíéúů\s]+$");
+
+            return reg.IsMatch(name);
+        }
+    }
+}
